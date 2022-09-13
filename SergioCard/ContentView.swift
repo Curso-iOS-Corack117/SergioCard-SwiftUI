778ca0b9d8e88eecbc4919a7ac535634d360da31
Ordaz Romero Sergio <orcheko@gmail.com>
2022-09-12 19:27:21 -0500

    Initial Commit

---
+++ SergioCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SergioCard
+//
+//  Created by Sergio Ordaz Romero on 12/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SergioCard/SergioCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SergioCardApp.swift
+//  SergioCard
+//
+//  Created by Sergio Ordaz Romero on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SergioCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
